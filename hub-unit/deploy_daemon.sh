f2ee391954fc54e26162d6c3e4d50b9b932fe744
MickeyH@BBGW <hozmickey@gmail.com>
2025-06-02 18:15:46 +0300

    refined some hub behaviors

--- hub-unit/deploy_daemon.sh
+++ hub-unit/deploy_daemon.sh
@@ -1,3 +1,4 @@
+sudo systemctl stop doors_hub
 sudo mkdir /usr/bin/doors_hub
 sudo cp build/* /usr/bin/doors_hub/
 sudo cp doors_hub.service /etc/systemd/system/doors_hub.service
--- hub-unit/door_manager/main.c
+++ hub-unit/door_manager/main.c
@@ -8,26 +8,43 @@
 #include "packet_utils.h"
 #include "i2c_register_defs.h"
 
-#define TARGET_ADDR_OFFSET 16
-#define TARGET_ADDR_COUNT 100
+#define TARGET_ADDR_OFFSET 0x30
+#define TARGET_ADDR_MAX_COUNT 0x0F
 
 static const char *device_path = "/dev/bone/i2c/2";
-static const uint8_t slave_addr_offset = 16;
-static const uint8_t slave_addr_count = 100;
 static const uint8_t polling_interval_sec = 1;
 
 static int device_fd = 0;
 
-bool target_addr_list[TARGET_ADDR_COUNT] = {0};
-bool target_addr_set[TARGET_ADDR_COUNT] = {false};
+static bool target_addr_set[TARGET_ADDR_MAX_COUNT] = {false};
+static uint8_t target_addr_list[TARGET_ADDR_MAX_COUNT] = {0};
+
+static uint8_t target_addr_last_count = 0;
 
 static uint8_t rx_buff[255] = {0};
 static DoorPacket_t packet_buff = {0};
 
 static void i2c_init(void)
 {
+	char buff[64] = {0};
+
+	sprintf(buff, "Opening I2C device at path %s", device_path);
+	syslog_append(buff);
+
 	// open the i2c device file
 	device_fd = open(device_path, O_RDWR, S_IWUSR);
+
+	if (device_fd <= 0)
+	{
+		sprintf(buff, "Error opening I2C device, code: %d", device_fd);
+		syslog_append(buff);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		sprintf(buff, "Successfully opened I2C device, descriptor: %d", device_fd);
+		syslog_append(buff);
+	}
 }
 
 static void i2c_set_target(uint8_t addr)
@@ -77,23 +94,26 @@ static void poll_slave_event_queue(void)
 
 	syslog_append("Starting polling");
 
-    for(;;)
+        for(;;)
 	{
+
         sleep(polling_interval_sec);
 
-        for (int i = 0; i < TARGET_ADDR_COUNT; i++)
+        for (int i = 0; i < target_addr_last_count; i++)
         {
-            if (target_addr_list[i] == 0) break;
             i2c_set_target(target_addr_list[i]);
 
             bzero(rx_buff, sizeof(rx_buff));
 
             // read event queue length
             // TODO: make this a uint16_t value?
-            i2c_master_read(I2C_REG_EVENT_COUNT, 1);
+            int32_t bytes_read = i2c_master_read(I2C_REG_EVENT_COUNT, 1);
             uint8_t queue_length = rx_buff[0];
+	    //sprintf(syslog_buff, "Read %ld bytes from address [0x%X] event count %u", bytes_read, target_addr_list[i], queue_length);
+	    //syslog_append(syslog_buff);
 
-            if (queue_length <= 0) continue;
+
+            if (bytes_read <= 0 || queue_length <= 0) continue;
         //		printf("Reported %u new events in slave queue! Reading...\n", queue_length);
 
             for (int i = 0; i < queue_length; i++)
@@ -190,32 +210,42 @@ static void poll_slave_event_queue(void)
 static void scan_i2c_bus(void)
 {
     char syslog_buff[32] = {0};
-    uint8_t list_index = 0;
     int32_t read_bytes = 0;
 
-    explicit_bzero(target_addr_set, TARGET_ADDR_COUNT);
-    explicit_bzero(target_addr_list, TARGET_ADDR_COUNT);
+    target_addr_last_count = 0;
+    explicit_bzero(target_addr_set, TARGET_ADDR_MAX_COUNT);
+    explicit_bzero(target_addr_list, TARGET_ADDR_MAX_COUNT);
 
     sprintf(syslog_buff, "Scanning I2C bus for target devices.");
     syslog_append(syslog_buff);
 
-    for (uint8_t i = 0; i < TARGET_ADDR_COUNT; i++)
+    for (uint8_t i = 0; i < TARGET_ADDR_MAX_COUNT; i++)
     {
         uint8_t addr = i+TARGET_ADDR_OFFSET;
         i2c_set_target(addr);
         read_bytes = i2c_master_read(I2C_REG_EVENT_COUNT, 1);
 
+	//    sprintf(syslog_buff, "Addr [0x%X], read bytes: %d", addr, read_bytes);
+	//    syslog_append(syslog_buff);
+
         if (read_bytes > 0)
         {
-            sprintf(syslog_buff, "Detected target device at address [0x%X].", addr);
-            syslog_append(syslog_buff);
             target_addr_set[i] = true;
-            target_addr_list[list_index] = addr;
-            list_index++;
+            target_addr_list[target_addr_last_count] = addr;
+            sprintf(syslog_buff, "Detected target device at address [0x%X].", target_addr_list[target_addr_last_count]);
+            syslog_append(syslog_buff);
+            target_addr_last_count += 1;
         }
     }
 
-    sprintf(syslog_buff, "Concluded I2C bus scan, %d devices detected.", list_index);
+    if (target_addr_last_count == 0)
+    {
+	    sprintf(syslog_buff, "Concluded I2C bus scan, no devices detected.");
+	    syslog_append(syslog_buff);
+	    exit(EXIT_FAILURE);
+    }
+
+    sprintf(syslog_buff, "Concluded I2C bus scan, %d devices detected.", target_addr_last_count);
     syslog_append(syslog_buff);
 }
 
@@ -227,6 +257,7 @@ int main(void)
     initialize_signal_handler();
     sprintf(buff, "Starting Door Manager, PID %u", getpid());
     syslog_append(buff);
+	sleep(1);
 	i2c_init();
 	sleep(1);
     scan_i2c_bus();
