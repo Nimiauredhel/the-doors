49a5ab4937a06fef9dcf49aaa2e4102fc4f728e1
MickeyH@BBGW <hozmickey@gmail.com>
2025-06-08 20:15:11 +0300

    [hub] server now successfully talking to intercom client

--- hub-unit/intercom_server/main.c
+++ hub-unit/intercom_server/main.c
@@ -12,7 +12,7 @@ static void server_init(void)
 
     char buff[64] = {0};
 
-    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (server_socket <= 0)
     {
@@ -39,18 +39,18 @@ static void server_init(void)
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(SERVER_PORT);
 
     client_addr.sin_family = AF_INET;
 
     int bind_result = bind(server_socket, (struct sockaddr *)&(server_addr), sizeof(server_addr));
 
-    if (bind_result < 0)
+    if (bind_result != 0)
     {
+        perror("Could not bind requests socket");
         sprintf(buff, "Could not bind requests socket");
         syslog_append(buff);
-        perror("Could not bind requests socket");
         exit(EXIT_FAILURE);
     }
 
@@ -58,10 +58,74 @@ static void server_init(void)
     syslog_append(buff);
 }
 
+void connection_loop(int data_socket, struct sockaddr* client_addr, socklen_t *addr_len)
+{
+	int ret;
+	char buff[64] = {0};
+	char comm_buff[64] = {0};
+
+	for(;;)
+	{
+		ret = recvfrom(data_socket, comm_buff, sizeof(comm_buff), 0, client_addr, addr_len);
+
+		if (ret <=0)
+		{
+			perror("Failed to receive on sub socket.");
+			sprintf(buff, "Failed to receive on sub socket.");
+			syslog_append(buff);
+			break;
+		}
+
+		sprintf(buff, "Received message: %s\n", comm_buff);
+		syslog_append(buff);
+
+		sprintf(comm_buff, "Hello back, this is server.");
+		ret = sendto(data_socket, comm_buff, strlen(buff)+1, 0, (struct sockaddr *)&client_addr, *addr_len);
+
+		if (ret <= 0)
+		{
+			perror("Failed to send to sub socket.");
+			sprintf(buff, "Failed to send to sub socket.");
+			syslog_append(buff);
+		}
+		else
+		{
+			perror("Sent reply to sub socket.");
+			sprintf(buff, "Sent reply to sub socket.");
+			syslog_append(buff);
+		}
+		sleep(1);
+	}
+}
+
 void server_loop(void)
 {
-    listen(server_socket, 10);
-    accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
+    char buff[64] = {0};
+
+    int ret = listen(server_socket, 10);
+
+    if (ret != 0)
+    {
+        perror("Failed to listen on socket.");
+        sprintf(buff, "Failed to listen on socket.");
+        syslog_append(buff);
+	return;
+    }
+
+    ret = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
+
+    if (ret < 0)
+    {
+        perror("Failed to accept on socket.");
+        sprintf(buff, "Failed to accept on socket.");
+        syslog_append(buff);
+	return;
+    }
+    else
+    {
+	int data_socket = ret;
+	connection_loop(data_socket, (struct sockaddr *)&client_addr, &addr_len);
+    }
     sleep(1);
 }
 
