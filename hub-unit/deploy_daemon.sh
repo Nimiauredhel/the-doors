9ec3eeacb1cef7c765fcd592cb9963d56b86fe61
MickeyH@BBGW <hozmickey@gmail.com>
2025-08-05 16:15:50 +0300

    [hub unit] slight tweaks to hub unit buid & deploy scripts in anticipation of a bigger change

--- hub-unit/deploy_daemon.sh
+++ hub-unit/deploy_daemon.sh
@@ -1,19 +1,7 @@
 sudo systemctl stop doors_hub
-sudo mkdir /usr/bin/doors_hub
 
-cd hub_control
-make strict
+sh rebuild_modules.sh
 
-cd ../door_manager
-make strict
-
-cd ../intercom_server
-make strict
-
-cd ..
-
-sudo cp build/* /usr/bin/doors_hub/
-sudo cp doors_hub.service /etc/systemd/system/doors_hub.service
 sudo systemctl daemon-reload
 sudo systemctl start doors_hub
 sudo systemctl enable doors_hub
