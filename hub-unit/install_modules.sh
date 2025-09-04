d71bc69072fef0e1651d762f165336d36d4c2fc4
MickeyH@BBGW <hozmickey@gmail.com>
2025-09-04 17:26:36 +0300

    [hub unit] deployment script now configures nginx to host a simple web page that will soon be used for telemetry, and later for administration

--- hub-unit/install_modules.sh
+++ hub-unit/install_modules.sh
@@ -4,3 +4,12 @@ sudo mkdir -p /usr/bin/doors_hub
 # copy all built modules to the installation folder
 sudo cp -r build/* /usr/bin/doors_hub/
 
+# stop nginx server if running
+sudo nginx -s stop
+
+# copy web interface
+sudo cp -r site /usr/bin/doors_hub/
+
+confpath="$(pwd)/nginx.conf"
+# set nginx profile
+sudo nginx -c "$confpath"
